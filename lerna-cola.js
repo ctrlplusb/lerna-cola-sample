d5735bb183506c9e1c265e02789260515dfdc8e9
Sean Matheson <sean@ctrlplusb.com>
2018-06-25 08:40:37 +0100

    Updates dependencies and adds start scripts.

--- lerna-cola.js
+++ lerna-cola.js
@@ -1,3 +1,16 @@
+const path = require('path')
+const dotenv = require('dotenv')
+const fs = require('fs')
+
+const dotenvFilePath = path.resolve(__dirname, './.env')
+
+if (fs.existsSync(dotenvFilePath)) {
+  dotenv.config({
+    path: dotenvFilePath,
+    encoding: 'utf8',
+  })
+}
+
 module.exports = {
   packages: {
     '@lerna-cola-sample/api-basket': {
--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,16 +1,3 @@
-const path = require('path')
-const dotenv = require('dotenv')
-const fs = require('fs')
-
-const dotenvFilePath = path.resolve(__dirname, '../../../.env')
-
-if (fs.existsSync(dotenvFilePath)) {
-  dotenv.config({
-    path: dotenvFilePath,
-    encoding: 'utf8',
-  })
-}
-
 module.exports = {
   servers: {
     basket: {
